df8592c390dd87d19778acab83fcbee48f7d56a4
Raț Adrian <91888446+adirat10@users.noreply.github.com>
2022-11-08 19:35:46 +0200

    Delete WebApplication_MVC directory

--- WebApplication_MVC/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace WebApplication_MVC.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
