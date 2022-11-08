90a117cbf224df0367b75cd43ddb38d7579f24fc
Raț Adrian <adirat10@gmail.com>
2022-11-08 19:06:43 +0200

    add person

---
+++ WebApp_MVC/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApp_MVC
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+        public string Country { get; set; }
+        public string Age { get; set; }
+        public string Ocupation { get; set; }
+
+    }
+}
