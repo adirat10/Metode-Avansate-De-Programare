51d3387ae5e8d9f3f39bf4efb53371f82045ee56
Raț Adrian <adirat10@gmail.com>
2022-11-15 19:28:13 +0200

    add people controller

---
+++ WebAppREST_API/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace WebAppREST_API.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+        public string Country { get; set; }
+        public string Ocupation { get; set; }
+        public string Age { get; set; }
+    }
+}
